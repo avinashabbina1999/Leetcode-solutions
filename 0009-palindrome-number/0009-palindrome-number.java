d736814df8058b5cbd3d77ba923d24e451f32d90
Avinash Abbina <avinashabbina1999@gmail.com>
2024-07-21 21:03:01 -0500

    Time: 5 ms (71.66%), Space: 43.9 MB (38.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int num = x;
+        int ans = 0;
+        while(num>0){
+            ans = ans*10 + num%10;
+            num/=10;
+        }
+        return x==ans;
+    }
+}

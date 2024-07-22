8b19645c9f6b60b874f771ff9896e8063381834b
Avinash Abbina <avinashabbina1999@gmail.com>
2024-07-21 20:53:54 -0500

    Time: 4 ms (94.09%), Space: 14.7 MB (11.87%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::map<int,int> map;
+        for(int i=0;i<nums.size();i++){
+            if(map.contains(target-nums[i]))
+                return {map[target-nums[i]],i};
+            map[nums[i]]=i;
+        }
+        return {-1,-1};
+    }
+};

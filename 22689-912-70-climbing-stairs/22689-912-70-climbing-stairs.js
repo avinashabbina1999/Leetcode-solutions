e300e10168bae7e18301dc899e7b64c0547bdbe0
Avinash Abbina <avinashabbina1999@gmail.com>
2025-01-22 10:46:38 -0600

    Time: 0 ms, Memory: 48.9 MB - LeetHub

---
+++ 22689-912-70-climbing-stairs/22689-912-70-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const dp = Array(n+1).fill(0);
+    dp[0]=1;
+    dp[1]=2;
+    for(let i=2;i<=n;i++){
+        dp[i] = dp[i-2]+dp[i-1];
+    }
+    return dp[n-1];
+};

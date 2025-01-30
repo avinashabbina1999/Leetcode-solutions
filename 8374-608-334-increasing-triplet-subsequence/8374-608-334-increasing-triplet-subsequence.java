0492a680df06c0a306a44670d666e734fec44739
Avinash Abbina <avinashabbina1999@gmail.com>
2025-01-30 12:14:41 -0600

    Time: 2 ms, Memory: 134.4 MB - LeetHub

---
+++ 8374-608-334-increasing-triplet-subsequence/8374-608-334-increasing-triplet-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int one = Integer.MAX_VALUE, two = Integer.MAX_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=one)
+                one = nums[i];
+            else if(nums[i]<=two)
+                two = nums[i];
+            else
+                return true;
+        }
+        return false;
+    }
+}

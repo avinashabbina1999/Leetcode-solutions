547255e06a1718df16eefed057bbbe687cbc3877
Avinash Abbina <avinashabbina1999@gmail.com>
2025-01-30 23:55:11 -0600

    Time: 5 ms, Memory: 57.8 MB - LeetHub

---
+++ 30490-1917-11-container-with-most-water/30490-1917-11-container-with-most-water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0,j = height.length-1,maxArea = 0;
+
+        while(i<j){
+            int area = (j-i)*Math.min(height[i],height[j]);
+            maxArea = Math.max(area,maxArea);
+
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        return maxArea;
+    }
+}

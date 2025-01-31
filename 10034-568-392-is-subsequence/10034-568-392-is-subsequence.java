d3638cbfa8d311243e6c7428712b748c827a0050
Avinash Abbina <avinashabbina1999@gmail.com>
2025-01-30 23:09:57 -0600

    Time: 1 ms, Memory: 41.5 MB - LeetHub

---
+++ 10034-568-392-is-subsequence/10034-568-392-is-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while(i<s.length()){
+            while(j<t.length() && s.charAt(i)!=t.charAt(j))
+                j++;
+            if(j==t.length())
+                return false;
+            i++;
+            j++;
+        }
+        return i==s.length();
+    }
+}
